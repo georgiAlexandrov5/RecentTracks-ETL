dcc0ce76b0771d141d7bb4d60b1be4a6259fb009
Georgi Alexandrov <grg_alexandrov@yahoo.com>
2023-05-28 23:13:25 +0300

    Adding validation/transformation

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 import requests
 import base64
 import datetime
+from sqlalchemy.orm import sessionmaker
+import sqlite3
+import pandas as pd
 
 # Set up the Spotify Developer credentials
-client_id = "c33d2440576744c2a3ac3a1423f23721"
-client_secret = "5ca6565b86d8485d8d8ff011156d7908"
-redirect_uri = "https://developer.spotify.com/dashboard"
+CLIENT_ID = "c33d2440576744c2a3ac3a1423f23721"
+CLIENT_SECRET = "5ca6565b86d8485d8d8ff011156d7908"
+REDIRECT_IRI = "https://developer.spotify.com/dashboard"
 
+DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
+
+#Data validation function
+def check_if_valid_data(df: pd.DataFrame) -> bool:
+    # Check if dataframe is empty
+    if df.empty:
+        print("No songs downloaded. Finishing execution")
+        return False
+
+    # Primary Key Check
+    if pd.Series(df['played_at']).is_unique:
+        pass
+    else:
+        raise Exception("Primary Key check is violated")
+
+    # Check for nulls
+    if df.isnull().values.any():
+        raise Exception("Null values found")
+
+
+    return True
 
 # Redirect user to authorize the application
-authorization_url = f"https://accounts.spotify.com/authorize?client_id={client_id}&response_type=code&redirect_uri={redirect_uri}&scope=user-read-recently-played"
+authorization_url = f"https://accounts.spotify.com/authorize?client_id={CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_IRI}&scope=user-read-recently-played"
 print("Please visit the following URL to authorize the application:")
 print(authorization_url)
 
@@ -18,14 +42,14 @@ authorization_code = input("Enter the authorization code from the callback URL: 
 
 # Exchange authorization code for access token
 auth_headers = {
-    "Authorization": "Basic " + base64.b64encode((client_id + ":" + client_secret).encode()).decode(),
+    "Authorization": "Basic " + base64.b64encode((CLIENT_ID + ":" + CLIENT_SECRET).encode()).decode(),
     "Content-Type": "application/x-www-form-urlencoded"
 }
 
 auth_data = {
     "grant_type": "authorization_code",
     "code": authorization_code,
-    "redirect_uri": redirect_uri
+    "redirect_uri": REDIRECT_IRI,
 }
 
 response = requests.post("https://accounts.spotify.com/api/token", headers=auth_headers, data=auth_data)
@@ -40,24 +64,39 @@ api_headers = {
 }
 
 today = datetime.datetime.now()
-yesterday = today - datetime.timedelta(days=10)
-yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
+timeframe = today - datetime.timedelta(days=10)
+timeframe_unix_timestamp = int(timeframe.timestamp()) * 1000
 
-r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp), headers = api_headers)
+r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=timeframe_unix_timestamp), headers = api_headers)
 response_data = r.json()
 
-# Process the response and extract relevant information
-if "items" in response_data:
-    for item in response_data["items"]:
-        track_name = item["track"]["name"]
-        artist_name = item["track"]["artists"][0]["name"]
-        album_name = item["track"]["album"]["name"]
-        played_at = item["played_at"]
+song_names = []
+artist_names = []
+played_at_list = []
+timestamps = []
 
-        print("Track:", track_name)
-        print("Artist:", artist_name)
-        print("Album:", album_name)
-        print("Played at:", played_at)
-        print("------------------------------")
-else:
-    print("No recently played songs found.")
+# Extracting only the relevant bits of data from the json object
+for song in response_data["items"]:
+    song_names.append(song["track"]["name"])
+    artist_names.append(song["track"]["album"]["artists"][0]["name"])
+    played_at_list.append(song["played_at"])
+    timestamps.append(song["played_at"][0:10])
+
+# Prepare a dictionary in order to turn it into a pandas dataframe below
+song_dict = {
+    "song_name": song_names,
+    "artist_name": artist_names,
+    "played_at": played_at_list,
+    "timestamp": timestamps
+}
+
+#Create a Pandas dateframe
+song_df = pd.DataFrame(song_dict, columns = ["song_name", "artist_name", "played_at", "timestamp"])
+pd.set_option('display.max_columns', 20)
+pd.set_option('display.width', 2000)
+
+print(song_df)
+
+#Validate
+if check_if_valid_data(song_df):
+        print("Data valid, proceed to Load stage")
