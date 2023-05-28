b5344096b6ab77c8a080c51a36d843497b225d4e
Georgi Alexandrov <grg_alexandrov@yahoo.com>
2023-05-28 23:48:54 +0300

    Loaded data to SQL table

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import base64
 import datetime
-from sqlalchemy.orm import sessionmaker
+import sqlalchemy
 import sqlite3
 import pandas as pd
 
@@ -95,8 +95,34 @@ song_df = pd.DataFrame(song_dict, columns = ["song_name", "artist_name", "played
 pd.set_option('display.max_columns', 20)
 pd.set_option('display.width', 2000)
 
-print(song_df)
 
 #Validate
 if check_if_valid_data(song_df):
         print("Data valid, proceed to Load stage")
+
+
+#Loading DF to SQLite
+
+engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+conn = sqlite3.connect('my_played_tracks.sqlite')
+cursor = conn.cursor()
+
+sql_query = """
+    CREATE TABLE IF NOT EXISTS my_played_tracks(
+        song_name VARCHAR(200),
+        artist_name VARCHAR(200),
+        played_at VARCHAR(200),
+        timestamp VARCHAR(200),
+        CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+    )
+    """
+
+cursor.execute(sql_query)
+
+try:
+    song_df.to_sql("my_played_tracks", engine, index=False, if_exists='append')
+except:
+    print("Data already exists in the database")
+
+conn.close()
+
